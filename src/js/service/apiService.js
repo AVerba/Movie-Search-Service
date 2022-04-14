270b0f371efc387bb7d6d6747bfb52b09c62d8d9
Oleksii Verbytskyi <alverba88@gmail.com>
2022-04-14 19:16:41 +0300

    added API settings adn service

---
+++ src/js/service/apiService.js
@@ -0,0 +1,60 @@
+const axios = require('axios'); 
+import apiSettings from './settings';
+const { API_KEY, BASE_URL }=apiSettings;
+
+export default class MovieApiService{
+    constructor() {
+        this.query = '';
+        this.page = 1;
+    }
+
+    async fetchTrendingMovie() {
+
+        const response = await axios.get(`${BASE_URL}trending/all/day?api_key=${API_KEY}&page=${this.page}`);
+        const data = await response.data;
+
+        return data;
+    }
+    async fetchSearchMovie() {
+        
+        const response = await axios.get(`${BASE_URL}search/movie?query=${this.query}&api_key=${API_KEY}&page=${this.page}`);
+        const data = await response.data;
+
+        return data;
+    }
+    async fetchDetailInfoMovie(movieId) {
+       
+        const response = await axios.get(`${BASE_URL}movie/${movieId}?api_key=${API_KEY}`);
+        const data = await response.data;
+
+        return data;
+    }
+
+    get searchQuery() {
+        return this.query;
+    }
+
+    set searchQuery(newQuery) {
+        this.query = newQuery;
+    }
+
+    incrementPage() {
+        this.page += 1;
+    }
+
+    resetPage() {
+        this.page = 1;
+    }
+}
+
+/* export const apiService=(url,options)=>{
+    return fetch(url,options).then(response=>{
+        if(!response.ok){
+            return response.json.then(error=>Promise.reject(error))
+        }
+         return response.json();
+    })
+} */
+
+
+
